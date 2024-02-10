29fabfd9409114bde4d666eaff5aad751e156102
Diego Santivañez <Diegojsf17@gmail.com>
2024-02-10 12:45:13 -0500

    Add files via upload

---
+++ src/test/java/com/example/thebigfunmicroservice/ThebigfunmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thebigfunmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebigfunmicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
